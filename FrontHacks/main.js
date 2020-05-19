470bf35036831a39612993fe8d8345a36e66c664
kazuto0291 <zutozuto03170291@gmail.com>
2020-05-19 11:40:07 +0900

    setTimuoutで非同期処理の順番を理解する
    スタックとキューの流れの理解のため

---
+++ FrontHacks/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+console.log(1);
+
+setTimeout (() => {
+  setTimeout(() => {
+    console.log(2);
+  }, 0);
+  console.log(3);
+},100);
+
+
+console.log(4);
+
+setTimeout(() => {
+  setTimeout(() => {
+    console.log(5);
+  }, 40);
+  console.log(6);
+}, 50);
+
+console.log(7);
+
+
+
+
+// 出力順は、1, 4, 7, 6, 3, 5 ,2
+
+//      正解 1, 4, 7, 6, 3, 2, 5
