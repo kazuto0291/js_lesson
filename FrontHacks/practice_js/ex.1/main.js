301e56eeb30bc27828cb67a76a9363650c3f428c
kazuto0291 <zutozuto03170291@gmail.com>
2020-05-22 00:28:02 +0900

    js

---
+++ FrontHacks/practice_js/ex.1/main.js
@@ -0,0 +1,35 @@
+let lastName = "文字列";
+let firstName = "ラストネーム";
+
+let fullName = firstName + " " +lastName;
+
+let myAge = 25;
+let friendAge =29;
+
+let totalAge = myAge + friendAge;
+
+let numberOfDays =totalAge * 365;
+
+let numberOfChildren = 3;
+
+let numberOfCakes = 10;
+
+let numberOfCakesByChild = numberOfCakes / numberOfChildren;
+
+let numberOfRestOfCake = numberOfCakes % numberOfChildren;
+
+
+console.log(lastName);
+console.log(firstName);
+console.log(fullName);
+console.log(myAge);
+console.log(friendAge);
+console.log(totalAge);
+console.log(numberOfDays);
+console.log(numberOfChildren);
+console.log(numberOfCakes);
+console.log(numberOfCakesByChild);
+console.log(numberOfRestOfCake);
+
+
+
