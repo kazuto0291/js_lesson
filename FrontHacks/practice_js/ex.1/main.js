d73d51d7edc774fba1e34b729948f3958d7f58d8
kazuto0291 <zutozuto03170291@gmail.com>
2020-05-22 10:13:12 +0900

    if-else-ifの復習

--- FrontHacks/practice_js/ex.1/main.js
+++ FrontHacks/practice_js/ex.1/main.js
@@ -1,3 +1,19 @@
+const weather = prompt('今日の天気は？次の3つのうち、いずれかの文字列を入力してください。(晴れ or 曇り or 雨)');
+
+
+if (weather === "晴れ") {
+  alert('今日は気持ちの良い一日になりそうですね！');
+} else if ( weather === "曇り") {
+  alert('どんよりした天気ですが今日も一日がんばりましょう！');
+} else if ( weather === "雨") {
+  alert('家でゆっくり過ごすのも良いと思いますよ！');
+} else {
+  alert('晴れ・くもり・雨以外の文字列には対応していません。');
+}
+
+
+
+
 let lastName = "文字列";
 let firstName = "ラストネーム";
 
