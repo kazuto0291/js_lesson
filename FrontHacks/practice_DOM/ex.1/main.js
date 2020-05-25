5e9f72136a82d0d64f6f7861316ca2ae00cd3b04
kazuto0291 <zutozuto03170291@gmail.com>
2020-05-26 00:36:00 +0900

    DOM取得方法５種類

--- FrontHacks/practice_DOM/ex.1/main.js
+++ FrontHacks/practice_DOM/ex.1/main.js
@@ -10,3 +10,26 @@ ulElement.removeChild(firstLiElement);
 const newLiElement = document.createElement('li');    //li要素のDOMを作成
 newLiElement.textContent = 'JSで作成したli要素';      //作ったli要素の文字列を定義する
 ulElement.appendChild(newLiElement);                //親要素Ulに作ったLi要素を追加する
+
+// -------------------------------------------------------------------
+
+const elementById =document.getElementById('title');
+console.log(elementById);
+
+const elementsByClassName = document.getElementsByClassName('item');  //クラス名'item'をすべて取得する
+console.log(elementsByClassName);
+
+const elementsByTagName = document.getElementsByTagName('li');    //liタグ要素をすべて取得する
+console.log(elementsByTagName);
+
+const h1ElementByQuerySelector = document.querySelector('#title');  //セレクタに一致したものを取得
+console.log(h1ElementByQuerySelector);
+
+const itemElementByQuerySelector = document.querySelector('.item'); //一番最初にあるセレクタ要素を１つのみ取得
+console.log(itemElementByQuerySelector);
+
+const h1ElementByQuerySelectorAll =document.querySelectorAll('#title'); //配列のようなNodeListを取得する
+console.log(h1ElementByQuerySelectorAll);
+
+const itemElementByQuerySelectorAll =document.querySelectorAll('.item');//すべての itemを取得する
+console.log(itemElementByQuerySelectorAll);
