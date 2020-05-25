de5939a95a195c8e01a1ada3708bd4fde56a635f
kazuto0291 <zutozuto03170291@gmail.com>
2020-05-25 20:01:39 +0900

    superを使うクラス継承
    superは継承関係にある子クラスと親クラスの内、子クラスからみた親クラスを指す。
    
    コンストラクタと、それ以外のメソッドでsuperの使い方が異なる。
    
    親クラスのコンストラクタを実行するときは子クラス内で、super()を実行。
    
    親クラスのコンストラクタ以外のメソッドを実行するときは、子クラス内で、super.メソッド名で実行する

--- FrontHacks/practice_js/ex.14_class_extends/main.js
+++ FrontHacks/practice_js/ex.14_class_extends/main.js
@@ -20,3 +20,33 @@ const dog =new Dog('ポチ');
 dog.bark();
 console.log(`getNameメソッド実行 : ${dog.getName()}`);
 console.log(`nameプロパティー値取得 : ${dog.name}`);
+
+
+//superを使うクラス継承
+
+class Fruits {
+  constructor(name) {
+    console.log('親クラスのコンストラクタ②');
+    this.name = name;
+  }
+  speak() {
+    console.log('親クラスのsuperメソッド③');
+  }
+}
+
+class Apple extends Fruits {
+  constructor(name, age) {
+    console.log('子クラスのコンストラクタ①');
+    super(name);
+    this.age = age;
+  }
+
+  speak() {
+    super.speak();
+    console.log('子クラスのsupperメソッド④');
+  }
+}
+
+const apple = new Apple('リンゴ', 2);   //①②
+apple.speak();                          //③④
+console.log(`名前；${apple.name} 、年齢： ${apple.age}⑤`);  //⑤
