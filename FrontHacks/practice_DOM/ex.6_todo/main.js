d4be47c2f874bcf8e791997ac7de36d76881ce77
kazuto0291 <zutozuto03170291@gmail.com>
2020-05-27 13:36:29 +0900

    showtodosの作成

--- FrontHacks/practice_DOM/ex.6_todo/main.js
+++ FrontHacks/practice_DOM/ex.6_todo/main.js
@@ -12,24 +12,49 @@ const buttonElement = document.getElementById('button');
 const UlElement =document.getElementById('todo-list');
 
 console.log(inputElement);
-console.log(buttonElement)
+console.log(buttonElement);
 console.log(UlElement);
 
   //「追加」ボタンがクリックされたときの処理を実装する
   //   - テキストボックスに入力されたテキストをTodoリスト一覧に追加する
   //   - テキストボックスの中を空にする
-
+buttonElement.addEventListener('click', (event) => {
+  const todo = inputElement.value;
+  inputElement.value = '';
+  if (todo) {
+    todos.push(todo);
+    showTodos();
+  }
+});
 
 
   // 「todos」の中身を一覧表示する
   //    - ul要素にli要素を追加して、li要素内にtodoタスクの内容を表示する
   //    - li要素内に削除ボタンを配置して、削除ボタンをクリックしたら対応するタスクを削除する
-
+const showTodos = () => {
+  while (UlElement.firstChild) {
+    UlElement.removeChild(UlElement.firstChild);
+  }
   
+  todos.forEach((todo, index) => {
+    const todoList = document.createElement('li');
+    const taskNumber = index + 1
+    todoList.textContent =`${taskNumber} : ${todo}`;
+    UlElement.appendChild(todoList);
+    
+  } )
+
+}
+
+
+
   // Todo情報を表すli要素(showTodo関数で作成される要素)の中にある削除ボタンをクリックしたら実行される。
   //   - todosから対応するtodo情報を削除する
   //   - 引数はindexを受け取る(インデックス番号)
   //   - 削除後はshowTodosを実行して、Todoリストを整理する
-
+const deleteTodo = (index) => {
+  todos.splice(index, 1);
+  showTodos();
+}
 
 })();
