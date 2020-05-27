abb09a3ad19aeae83f4c811776ccfd8d6ef946cf
kazuto0291 <zutozuto03170291@gmail.com>
2020-05-27 18:56:26 +0900

    非同期関数を使ったthenの処理の流れ
    thenメソッド内で非同期処理を実装したいときは、return に「new Promise(() =>{})」をセットする

--- FrontHacks/ Asynchronous processing/Promise_then/main.js
+++ FrontHacks/ Asynchronous processing/Promise_then/main.js
@@ -19,4 +19,34 @@ promise
   })
   .then((value) => {
     console.log(`引数の値はreturnしてないから帰ってこない：${value}`);
+  });
+// ===================================================================
+
+
+
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(800);
+  }, 1000);
+});
+
+promise1
+  .then((value) => {
+    console.log(`１つ目のthenの引数：${value}`);
+    return new Promise((resolve, reject) => {       //thenの中で非同期処理
+      setTimeout(() => {
+        resolve(1000);
+      }, 1000);
+    });
   })
+  .then((value) => {
+    console.log(`2つ目のthenの引数：${value}`);
+    return new Promise((resolve, reject) => {   //thenの中で非同期処理
+      setTimeout(() => {
+        resolve(1500);
+      }, 1000);
+    });
+  })
+  .then((value) => {
+    console.log(`3つ目のthenの引数：${value}`);
+  });
