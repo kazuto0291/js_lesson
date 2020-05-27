845ecd151b6b2fdc5ea54ec6cfaed51bdd567624
kazuto0291 <zutozuto03170291@gmail.com>
2020-05-27 19:18:22 +0900

    thenメソッド内で例外処理、throw
    thenメソッド内でthrowを浮かう場面
    ・意図しない値が取得されたとき
    ・API通信がうまく行かなかった
    などの理由で、処理を中断したいときに使う

--- FrontHacks/ Asynchronous processing/Promise_catch/main.js
+++ FrontHacks/ Asynchronous processing/Promise_catch/main.js
@@ -15,3 +15,24 @@ promise
   .catch((error) => {
     console.log(error.message, '@@@@@@@@')
   })
+
+
+const promise1 = Promise.resolve(1);
+
+promise1
+  .then(() => {
+    console.log(1);
+  })
+  .then(() => {
+    console.log(2);
+  })
+  .then(() => {
+    throw 'errorです'
+    console.log(3);
+  })
+  .then(() => {
+    console.log(4);
+  })
+  .catch((error) => {
+    console.log(error, '@@@@@@@@@')
+  })
