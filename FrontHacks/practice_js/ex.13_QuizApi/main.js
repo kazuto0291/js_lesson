a2a13b0c54eef7e5463971b1d9b44b8b2f12c47d
kazuto0291 <zutozuto03170291@gmail.com>
2020-05-28 16:44:02 +0900

    作成したクラスメソッドを実行して、APIの結果を出力

--- FrontHacks/practice_js/ex.13_QuizApi/main.js
+++ FrontHacks/practice_js/ex.13_QuizApi/main.js
@@ -11,16 +11,33 @@ const API_URL = 'https://opentdb.com/api.php?amount=10';
 
 class QuizFetcher {
   static fetchMultipleQuizData() {
-
+    const url = `${API_URL}&type=multiple`;
+    return fetch(url)
+      .then(response => {
+        return response.json();
+      });
   }
   static fetchTrueOrFalseQuizData() {
-    
+    const url = `${API_URL}&type=boolean`
+    return fetch(url)
+      .then(response => {
+        return response.json();
+      })
   }
 }
 
 
 // 課題2: fetchMultipleQuizDataメソッドを実行してAPIの結果に含まれるresultsプロパティの値をconsole.logで出力する
-
+QuizFetcher
+  .fetchMultipleQuizData()
+  .then((data) => {
+    console.log(data.results)
+  })
 
 
 // 課題3: fetchTrueOrFalseQuizDataメソッドを実行してAPIの結果に含まれるresultsプロパティの値をconsole.logで出力する
+QuizFetcher
+  .fetchTrueOrFalseQuizData()
+  .then((data) => {
+    console.log(data.results);
+  })
