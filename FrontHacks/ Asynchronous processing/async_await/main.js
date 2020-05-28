1b0089148027512e0bf4f406afb437cd5d344ebb
kazuto0291 <zutozuto03170291@gmail.com>
2020-05-29 02:16:01 +0900

    Update main.js

--- FrontHacks/ Asynchronous processing/async_await/main.js
+++ FrontHacks/ Asynchronous processing/async_await/main.js
@@ -1,4 +1,18 @@
-const func = async () => {
-  await
+const addAfter3Seconds =  (x, y) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(x + y);
+    }, 3000)
+  })
 }
-console.log(func());
+
+const promise = addAfter3Seconds(1,2);
+promise.then(value => {
+  console.log(value, '@@@@@@');
+})
+
+
+const callAsyncFunc = async () => {
+  const value = await addAfter3Seconds(1, 2);
+  console.log(value, '@@@@@@@@@@@@')
+}
