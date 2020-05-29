db7485a5a622fdf036990264048e45248026cb21
kazuto0291 <zutozuto03170291@gmail.com>
2020-05-29 20:26:15 +0900

    node.jsでJavaScriptファイルを実行する

--- FrontHacks/node.js/node1/main.js
+++ FrontHacks/node.js/node1/main.js
@@ -1,5 +1,13 @@
-const fs = require('fs')
+// const fs = require('fs')
 
-fs.mkdir('./apple2', {recursive: true}, (err) => {
-  if (err) throw err;
-});
+// fs.mkdir('./apple2', {recursive: true}, (err) => {
+//   if (err) throw err;
+// });
+
+function add(x, y) {
+  return x * y;
+}
+
+console.log('add(2, 3)', add(2, 3));
+console.log('add(4, 5)', add(4, 5));
+console.log('add(6, 7)', add(6, 7));
