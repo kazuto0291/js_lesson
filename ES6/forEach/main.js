e53aced96ca177d4ef5e000570b05afa76b381fe
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-15 00:01:02 +0900

    forEach

--- ES6/forEach/main.js
+++ ES6/forEach/main.js
@@ -3,3 +3,11 @@ const colors = [ 'red', 'blue', 'green'];
 for (let i = 0; i < colors.length; i++) {
   console.log(colors[i]);
 }
+
+colors.forEach(function(color) {
+  console.log(color);
+})
+
+colors.forEach((color, index) => {
+  console.log(`${index + 1}:${color}`)
+})
