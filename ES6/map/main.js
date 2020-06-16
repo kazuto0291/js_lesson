2a9dcbed7c47ae3000efb496a4030602009dc3d1
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-16 23:35:42 +0900

    mapー興味のある情報のみを選択する

--- ES6/map/main.js
+++ ES6/map/main.js
@@ -17,3 +17,20 @@ doubledNumbers;
 
 console.log(doubled);
 console.log(doubledNumbers);
+
+// ======================================
+
+const cars = [
+  {type: '軽自動車', price: '安い'},
+  {type: '高級車', price: '高い'},
+  {type: '自動車', price: '普通'},
+  {type: '原付き', price: '安い'},
+  {type: '中型', price: '普通'},
+  {type: '大型', price: '高い'}
+];
+
+const prices = cars.map(function(car) {
+  return car.price;
+})
+
+console.log(prices);
