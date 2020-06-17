be4672a6a48ce22bd45b99826997c21d8fc8b040
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-17 23:45:31 +0900

    条件を絞り込む　filter

--- ES6/filter/main.js
+++ ES6/filter/main.js
@@ -1,11 +1,11 @@
 const products = [
-  { name: 'きゅうり', type: '野菜'},
-  { name: 'トマト', type: '野菜'},
-  { name: 'メロン', type: '果物'},
-  { name: 'バナナ', type: '果物'},
-  { name: 'ほうれん草', type: '野菜'},
-  { name: 'みかん', type: '果物'},
-  { name: 'きゅうり', type: '野菜'}
+  { name: 'きゅうり', type: '野菜', quantity: 0, price: 1 },
+  { name: 'トマト', type: '野菜' , quantity: 14, price: 15},
+  { name: 'メロン', type: '果物', quantity: 30, price: 8 },
+  { name: 'バナナ', type: '果物', quantity: 45, price: 20},
+  { name: 'ほうれん草', type: '野菜', quantity: 35, price: 35},
+  { name: 'みかん', type: '果物', quantity: 15, price: 5},
+  { name: 'きゅうり', type: '野菜', quantity: 80, price: 20}
 ];
 
 const filteredProducts = [];
@@ -23,3 +23,13 @@ const filterProducts2 = products.filter(function(product) {
 })
 
 console.log(filterProducts2);
+
+
+const filterProducts3 = products.filter(function(product) {
+  return product.type === '果物'
+      && product.quantity > 0
+      && product.price < 10;
+});
+
+console.log(filterProducts3);
+
