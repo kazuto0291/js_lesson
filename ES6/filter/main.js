d9d1ea5335ea7861ba5dfa2f26af9b5983942b8c
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-18 00:03:59 +0900

    演習２

--- ES6/filter/main.js
+++ ES6/filter/main.js
@@ -62,3 +62,19 @@ const filteredNumbers = numbers.filter((number) => {
 })
 
 console.log(filteredNumbers);
+
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+
+const users = [
+  {id: 1, admin: true },
+  {id: 2, admin: false },
+  {id: 3, admin: false },
+  {id: 4, admin: false },
+  {id: 5, admin: true }
+];
+
+const filteredUsers = users.filter((user) => {
+  return user.admin === true;
+})
+
+console.log(filteredUsers);
