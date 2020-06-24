f5874784db22ce7792db05b01d4b7d271be62e55
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-24 23:10:17 +0900

    find　演習

--- ES6/find/main.js
+++ ES6/find/main.js
@@ -66,3 +66,17 @@ const admin = members.find((member) => {
 });
 
 console.log(admin);
+
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+
+const accounts = [
+  {balance: -10 },
+  {balance: 12 },
+  {balance: 0 }
+]
+
+const account = accounts.find((account) => {
+  return account.balance === 12;
+});
+
+console.log(account);
