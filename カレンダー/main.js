0f20b977bd8e98e153a475895954c1f4dd52b533
kazuto0291 <zutozuto03170291@gmail.com>
2020-07-07 21:54:58 +0900

    前月分の日付の取得

--- カレンダー/main.js
+++ カレンダー/main.js
@@ -5,6 +5,20 @@ console.clear();
   const year = 2020;
   const month = 6
 
+  function getCalendarHead() {
+    const dates= [];
+    const d = new Date(year, month, 0).getDate();
+    const n = new Date(year, month, 1).getDay();
+
+    for (let i = 0; i < n; i++) {
+      dates.unshift({
+        date: d -i,
+        isToday: false,
+        isDisabled: true,
+      });
+    }
+    console.log(dates);
+  }
 
   function getCalendarBody() {
     const dates = []; //date:日付、day:曜日
@@ -20,5 +34,5 @@ console.clear();
     console.log(dates);
   }
 
-  getCalendarBody();
+  getCalendarHead();
 }
