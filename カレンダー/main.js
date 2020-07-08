a20935becd4a4d502cce952709b17c1c16b08db7
kazuto0291 <zutozuto03170291@gmail.com>
2020-07-08 23:15:50 +0900

    cakendar

--- カレンダー/main.js
+++ カレンダー/main.js
@@ -33,6 +33,22 @@ console.clear();
     }
     console.log(dates);
   }
+  
+  function getCalendarTail() {
+    const dates = [];
+    const lastDay = new Date(year, month + 1 , 0).getDay();
 
-  getCalendarHead();
+    for (let i = 1; i < 7 - lastDay; i++) {
+      dates.push({
+        date: i,
+        isToday: false,
+        isDisabled:true,
+      })
+    }
+    console.log(dates);
+  }
+
+  getCalendarTail();
+
+  
 }
