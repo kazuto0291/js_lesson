45b9c2b6d3eb5282fc5bd4213e00aa337c0035f5
kazuto0291 <zutozuto03170291@gmail.com>
2020-07-16 23:25:52 +0900

    every

---
+++ ES6/every_some/main.js
@@ -0,0 +1,26 @@
+const computers = [
+  {name: 'Apple', ram: 24},
+  {name: 'Compaq', ram: 4},
+  {name: 'Acer', ram: 32}
+];
+
+// 16GB ramが必要なソフト
+
+// 全てで動かせるフラグ
+const allComputersCanRun = true;
+
+// どれかでは動かせるフラグ
+const someComputersCanRun = false;
+
+for (let i = 0; i < computers.lengtu; i++) {
+  const computer = computers[i];
+
+  if (computer.ram < 16) {
+    allComputersCanRun = false;
+  } else {
+    someComputersCanRun = true;
+  }
+}
+
+console.log(allComputersCanRun);
+console.log(someComputersCanRun);
