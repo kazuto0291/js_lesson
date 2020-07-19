e6bf2c1af2b8216f722bfb2996b44371508e088a
kazuto0291 <zutozuto03170291@gmail.com>
2020-07-19 23:50:44 +0900

    reduce

--- ES6/reduce/main.js
+++ ES6/reduce/main.js
@@ -20,6 +20,16 @@ const preEx = primaryColors.reduce(function(previous, primaryColor) {
 },[])
 
 console.log(preEx, "@")
+// ============================
+function balancedParens(string) {
+  return string.split('').reduce(function(previous, char) {
+    if (previous < 0) {return previous;}
+    if (char === '(') { return previous + 1;}
+    if (char === ')') {return previous - 1}
+  }, 0);
+}
 
+const ex =balancedParens('()(((((');
 
+console.log(ex);
 
