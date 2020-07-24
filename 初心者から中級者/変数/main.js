57cf583596edb7a85a094022eed3642cd37b03c0
kazuto0291 <zutozuto03170291@gmail.com>
2020-07-24 22:31:18 +0900

    参照と引数

--- 初心者から中級者/変数/main.js
+++ 初心者から中級者/変数/main.js
@@ -1,16 +1,27 @@
-function hello(name) {
-  // if (!name) {
-  //   name = 'Tom'
-  // }
-  name = name || 'Tom';
-  console.log(`hello ${name}`);
+let a = 0;
+
+function fn1(arg1) {
+  arg1 = 1;
+  console.log(a, arg1);
 }
 
-hello('Bob');
+fn1(a);
 
-let name = 'Bob';
-if (name) {
-  hello(name);
+let b = {
+  prop: 0
 }
 
-name && hello(name);
+function fn2(arg2) {
+  arg2.prop = 1;
+  console.log(b, arg2);
+}
+
+fn2(b);
+
+
+function fn3(arg2) {
+  arg2 = {};
+  console.log(b, arg2);
+}
+
+fn3(b);
