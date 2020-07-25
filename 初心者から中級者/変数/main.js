c4d890c455742a232a4e9d0410f2ec3ebde2c9ac
kazuto0291 <zutozuto03170291@gmail.com>
2020-07-25 21:12:52 +0900

    Update main.js

--- 初心者から中級者/変数/main.js
+++ 初心者から中級者/変数/main.js
@@ -1,27 +1,15 @@
-let a = 0;
-
-function fn1(arg1) {
-  arg1 = 1;
-  console.log(a, arg1);
-}
-
-fn1(a);
-
-let b = {
+const a = {
   prop: 0
 }
 
-function fn2(arg2) {
-  arg2.prop = 1;
-  console.log(b, arg2);
+const b = {
+  prop: 0
 }
 
-fn2(b);
+console.log(a === b);
 
+console.log(a.prop === b.prop);
 
-function fn3(arg2) {
-  arg2 = {};
-  console.log(b, arg2);
-}
+const c = a ;
 
-fn3(b);
+console.log(a === c);
