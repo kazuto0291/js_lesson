5d4887f75da70ac13918409aa0311578c24ec343
kazuto0291 <zutozuto03170291@gmail.com>
2020-07-28 00:35:01 +0900

    sql

--- WEB/mysql_lessons/myapp.sql
+++ WEB/mysql_lessons/myapp.sql
@@ -1,22 +1,63 @@
 drop table if exists users;
 create table users (
-  id int unsigned,
-  name varchar(20),
-  score float default 0
+  id int unsigned primary key auto_increment,
+  name varchar(20)unique,
+  score float
 );
 
-insert into users (id, name, score) values (1, 'taguchi', 5.8);
-insert into users (id, name, score) values (2, 'nakajima', 6.4);
-insert into users (id, name, score) values (3, 'koizumi', 6.3);
-insert into users (id, name, score) values (4, 'yamada', null);
-insert into users (id, name, score) values (5, 'tanaka', 0);
+insert into users (name, score) values ('taguchi', 5.8);
+insert into users (name, score) values ('fkoji', 8.2);
+insert into users (name, score) values ('dotinstll', 6.1);
+insert into users (name, score) values ('Tanaka', 4.2);
+insert into users (name, score) values ('yamada', null);
+insert into users (name, score) values ('tashiro', 7.9);
+
+select * from users order by score;
+select * from users where score is not null order by score desc;
+select * from users where score is not null order by score desc limit 3;
+select * from users where score is not null order by score desc limit 3 offset 3;
+
+-- select * from users where name like 't%';
+-- select * from users where name like '%a%';
+-- select * from users where name like '%a';
+-- select * from users where name like binary 'T%';
+-- select * from users where name like '______';
+-- select * from users where name like '_a%';
+
+-- select * from users where score >= 3 and score <= 6;
+-- select * from users where score between 3.0 and 6.0;
+-- select * from users where name = 'taguchi' or name = 'fkoji';
+-- select * from users where name in ('taguchi' , 'fkoji');
+
+
+-- alter table users add column email varchar(255) after name;
+-- alter table users drop column score;
+-- alter table users change name user_name varchar(80) default 'nobody';
+
+-- desc users;
+
+-- alter table users rename persons;
+-- show tables;
+-- drop table if exists persons;
+
+-- insert into users (name, score) values ('taguchi', 5.8);
+-- insert into users (name, score) values ('nakajima', 6.4);
+-- insert into users (name, score) values ('koizumi', 6.3);
+-- insert into users (name, score) values ('yamada', null);
+-- insert into users (name, score) values ('tanaka', 0);
+-- insert into users (name, score) values ('tan', 0);
+-- insert into users (id, name, score) values (1, 'taguchi', 5.8);
+-- insert into users (id, name, score) values (2, 'nakajima', 6.4);
+-- insert into users (id, name, score) values (3, 'koizumi', 6.3);
+-- insert into users (id, name, score) values (4, 'yamada', null);
+-- insert into users (id, name, score) values (5, 'tanaka', 0);
+-- insert into users (id, name, score) values (5, 'tan', 0);
 -- insert into users (id, name, score) values
 --   (1, 'taguchi', 5.8),
 --   (2, 'nakajima', 6.4),
 --   (3, 'koizumi', 6.3),
 --   (4, 'yamada', null);
 
-select * from users;
 /*
 number:
 - int
