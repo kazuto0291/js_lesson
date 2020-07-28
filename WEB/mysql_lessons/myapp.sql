920aa1e52a34dcbeae8ee1d0c355bc6cb139f710
kazuto0291 <zutozuto03170291@gmail.com>
2020-07-28 21:44:06 +0900

    sql

--- WEB/mysql_lessons/myapp.sql
+++ WEB/mysql_lessons/myapp.sql
@@ -3,6 +3,8 @@ create table users (
   id int unsigned primary key auto_increment,
   name varchar(20)unique,
   score float
+  -- rank enum('gold', 'silver', 'bronze')
+  -- coins set('gold', 'silver', 'bronze')
 );
 
 insert into users (name, score) values ('taguchi', 5.8);
@@ -12,10 +14,39 @@ insert into users (name, score) values ('Tanaka', 4.2);
 insert into users (name, score) values ('yamada', null);
 insert into users (name, score) values ('tashiro', 7.9);
 
-select * from users order by score;
-select * from users where score is not null order by score desc;
-select * from users where score is not null order by score desc limit 3;
-select * from users where score is not null order by score desc limit 3 offset 3;
+select * from users;
+
+start transaction;
+update users set score = score - 1.2 where name = 'fkoji';
+update users set score = score + 1.2 where name = 'taguchi';
+-- commit;
+rollback;
+-- select length(name) as len, name from users order by len;
+
+-- select length('hello');
+-- select substr('hello', 2);
+-- select substr('hello', 2, 3);
+-- select upper('hello');
+-- select lower('hello');
+-- select concat('hello', 'world');
+-- update users set score = 5.9;
+-- update users set score = 5.9 where id =1;
+-- update users set name = 'sasaki', score = 2.9 where name ='tanaka';
+-- delete from users where score < 5.0;
+-- update users set score = score * 1.2 where id % 2 = 0;
+
+-- select round(5.355);
+-- select round(5.355, 1);
+-- select floor(5.833);
+-- select ceil(5.238);
+-- select rand();
+-- select * from users order by rand() limit 1;
+select * from users;
+
+-- select * from users order by score;
+-- select * from users where score is not null order by score desc;
+-- select * from users where score is not null order by score desc limit 3;
+-- select * from users where score is not null order by score desc limit 3 offset 3;
 
 -- select * from users where name like 't%';
 -- select * from users where name like '%a%';
