24d0f30418f6ea76d32b989055ce3f27958d4d59
kazuto0291 <zutozuto03170291@gmail.com>
2020-08-02 13:17:31 +0900

    ディレクティブ：v-bind,v-on

--- vue/first/main.js
+++ vue/first/main.js
@@ -2,13 +2,15 @@ new Vue({
   el: '#app',
   data() {
     return {
-      title: 'titleデータ',
-      body: 'bodyです。'
+      url: 'https://google.com'
     }
   },
   methods: {
     showDate() {
       return new Date().toISOString()
+    },
+    clickButton() {
+      alert('クリック!⚠');
     }
   }
 })
