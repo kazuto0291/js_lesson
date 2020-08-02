6e25cf885a3f91f6d69ed6ca729f14c574d54629
kazuto0291 <zutozuto03170291@gmail.com>
2020-08-02 14:50:53 +0900

    computedtomethodsの動作の違い
    対象となるdataの更新されたときだけ処理を実行したい場合は、computedを使うと不要なメソッド呼び出しをせずに住む

---
+++ vue/computed/main.js
@@ -0,0 +1,29 @@
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      text1: '',
+      text2: ''
+    }
+  },
+  computed: {
+    capitalizedText1() {
+      console.log('computed.');
+      if (!this.text1) {
+        return '';
+      }
+
+      return this.text1.charAt(0).toUpperCase() + this.text1.slice(1);
+    }
+  },
+  methods: {
+    capitalizeText2() {
+      console.log('methods.');
+      if (!this.text2) {
+        return '';
+      }
+
+      return this.text2.charAt(0).toUpperCase() + this.text2.slice(1);
+    }
+  }
+})
