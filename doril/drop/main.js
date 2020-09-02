895348dbfbc2bd516d30fcd83265e89204245588
kazuto0291 <zutozuto03170291@gmail.com>
2020-09-03 01:57:52 +0900

    reset

--- doril/drop/main.js
+++ doril/drop/main.js
@@ -1,17 +1,4 @@
-function drop(array, size = 1) {
-  if (size === 0) {
-    return [...array];
-  }
-  if (array.lenght <= size) {
-    return [];
-  }
 
-  const droppedArray = [];
-  for (let i = size; i < array.length; i++) {
-    droppedArray.push(array[i]);
-  }
-  return droppedArray;
-}
 
 console.log(drop([1,2,3]));
 
