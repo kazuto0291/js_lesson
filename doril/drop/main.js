22c061a406d7aaec2a7715e487a234f069886409
kazuto0291 <zutozuto03170291@gmail.com>
2020-09-03 02:17:14 +0900

    drop

--- doril/drop/main.js
+++ doril/drop/main.js
@@ -1,3 +1,20 @@
+function drop(array, size = 1) {
+
+  if (size === 0) {
+    return [...array];
+  }
+
+
+  if (array.length <= size) {
+    return [];
+  }
+
+  const droppedArray = [];
+  for(let i = size; i < array.length; i++) {
+    droppedArray.push(array[i]);
+  }
+  return droppedArray;
+}
 
 
 console.log(drop([1,2,3]));
