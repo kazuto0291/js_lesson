c75a3b6427648ee2f04cd9f9c2deda98fc07ff69
kazuto0291 <zutozuto03170291@gmail.com>
2020-09-06 21:27:24 +0900

    indexOf

---
+++ doril/indexOf/main.js
@@ -0,0 +1,20 @@
+function indexOf(array, value, fromIndex=0) {
+
+  for(let i = fromIndex; i < array.length; i++ ) {
+    if(array[i] === value) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+
+console.log( indexOf([1, 2, 1, 2], 2) );
+// => 1
+
+// Search from the `fromIndex`.
+console.log( indexOf([1, 2, 1, 2], 2, 2) );
+// => 3
+console.log( indexOf([1, 2, 1, 2], 3, 2) );
+// => -1
