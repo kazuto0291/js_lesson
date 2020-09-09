b589f0ea9c189e7f2976e5173bd1dca411312750
kazuto0291 <zutozuto03170291@gmail.com>
2020-09-10 00:26:54 +0900

    tail

---
+++ doril/tail/main.js
@@ -0,0 +1,9 @@
+function tail(array) {
+  const tailArray = [];
+  for(let i = 1; i < array.length; i++) {
+    tailArray.push(array[i]);
+  }
+  return tailArray;
+}
+
+console.log(tail([1, 2, 3]));
