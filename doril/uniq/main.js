5e624990a8b8239725b72e1b5adc2d2b04a9f9e1
kazuto0291 <zutozuto03170291@gmail.com>
2020-09-12 23:08:11 +0900

    uniq

---
+++ doril/uniq/main.js
@@ -0,0 +1,19 @@
+function uniq(array) {
+  const uniqArray = [];
+  for(let i = 0; i <array.length; i++) {
+    const value = array[i];
+    if( !uniqArray.includes(value) ) {
+      uniqArray.push( value );
+    }
+  }
+  return uniqArray;
+}
+
+
+const numbers = [2, 1, 2];
+
+console.log( uniq(numbers) );
+// => [2, 1]
+
+console.log( numbers );
+// => [2, 1, 2]
