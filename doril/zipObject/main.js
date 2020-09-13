b35299166355255a649c096d5ec35c44a23601ec
kazuto0291 <zutozuto03170291@gmail.com>
2020-09-13 10:30:59 +0900

    zipobject

---
+++ doril/zipObject/main.js
@@ -0,0 +1,14 @@
+function zipObject(props = [], values = []) {
+  const zippedObject = {};
+  for(let i = 0; i < props.length; i++) {
+    const prop = props[i];
+    const value = values[i];
+    zippedObject[prop] = value;
+  }
+  
+  return zippedObject;
+}
+
+
+console.log( zipObject(['a', 'b'], [1, 2]) );
+// => { 'a': 1, 'b': 2 }
