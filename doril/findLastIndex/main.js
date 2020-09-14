a2dce1e0b17ac266444f1cfe0ddba7afcc09f781
kazuto0291 <zutozuto03170291@gmail.com>
2020-09-15 01:09:25 +0900

    findLastindex

---
+++ doril/findLastIndex/main.js
@@ -0,0 +1,37 @@
+function findLastIndex(array, predicate, fromIndex = array.length - 1) {
+  for(let i = fromIndex; 0 <= i; i--) {
+    const value = array[i];
+    if( predicate(value)) {
+      return i;
+    }
+  }
+
+  return -1
+}
+
+
+const users = [
+  { 'user': 'pebbles', 'active': false },
+  { 'user': 'barney',  'active': true },
+  { 'user': 'john',  'active': true },
+  { 'user': 'fred',    'active': false },
+  { 'user': 'pebbles', 'active': false }
+];
+
+const result1 = findLastIndex(users, function(o) {
+  return o.user === 'pebbles';
+});
+console.log(result1);
+// => 4
+
+const result2 = findLastIndex(users, function(o) {
+  return o.user === 'pebbles';
+}, 2);
+console.log(result2);
+// => 0
+
+const result3 = findLastIndex(users, function(o) {
+  return o.user === 'john';
+});
+console.log(result3);
+// => 2
