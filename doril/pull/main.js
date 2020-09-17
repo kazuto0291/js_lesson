bfa923c4442243c1166ac450a61d83063ae5f783
kazuto0291 <zutozuto03170291@gmail.com>
2020-09-18 01:53:41 +0900

    pull

---
+++ doril/pull/main.js
@@ -0,0 +1,16 @@
+function pull(array, ...values) {
+  for(let i = array.length - 1; 0 <= i; i--) {
+    if( values.includes( array[i] )) {
+      array.splice(i, 1);
+    }
+  }
+  return array
+}
+
+
+
+
+const array = ['a', 'b', 'c', 'a', 'b', 'c'];
+pull(array, 'a', 'c');
+console.log(array);
+// => ['b', 'b']
