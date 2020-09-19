4a4d1f2656e16a159fa01a56cdbc4941fb826a6a
kazuto0291 <zutozuto03170291@gmail.com>
2020-09-19 21:50:11 +0900

    takeRightWhile

---
+++ doril/takeRightWhile/main.js
@@ -0,0 +1,30 @@
+function takeRightWhile(array, predicate) {
+  const takenArray = [];
+  for(let i = array.length - 1; 0<=i ; i--) {
+    const value = array[i];
+    if( !predicate(value) ) {
+      break;
+    }
+    takenArray.unshift(value);
+  }
+
+  return takenArray;
+}
+
+
+
+const users = [
+  { 'user': 'barney',  'active': true },
+  { 'user': 'fred',    'active': false },
+  { 'user': 'pebbles', 'active': false }
+];
+
+const result = takeRightWhile(users, function(o) {
+  return !o.active;
+});
+
+console.log(result);
+// => objects for ['fred', 'pebbles']
+
+console.log(users);
+// => objects for ['barney', 'fred', 'pebbles']
