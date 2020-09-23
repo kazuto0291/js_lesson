197cda19e73afc8d38991ee308f0a466cf3dd67c
kazuto0291 <zutozuto03170291@gmail.com>
2020-09-24 01:31:09 +0900

    compact

---
+++ doril/doril2/compact.js
@@ -0,0 +1,16 @@
+function compact(array) {
+  const compactedArray = []
+
+  for(let i = 0; i < array.length ; i++) {
+    const target = array[i]
+    if(target) {
+      compactedArray.push(target);
+    }
+  }
+
+  return compactedArray
+}
+
+const compactedValues = compact([0, 1, false, 2, '', 3, 4, false, '', 1000]);
+console.log(compactedValues);
+// => [1, 2, 3]
