a040d5b11d0b7628d447884d2325132586814d73
kazuto0291 <zutozuto03170291@gmail.com>
2020-10-01 03:00:56 +0900

    re;obj

--- doril/doril2/zipObject.js
+++ doril/doril2/zipObject.js
@@ -1,5 +1,12 @@
-
-
+function zipObject(array, values) {
+  const zipobj = {};
+  for(let i = 0; i < array.length; i ++) {
+    const target = array[i]
+    const value = values[i]
+    zipobj[target] =value
+  }
+  return zipobj;
+}
 
 console.log( zipObject(['a', 'b'], [1, 2]) );
 // => { 'a': 1, 'b': 2 }
