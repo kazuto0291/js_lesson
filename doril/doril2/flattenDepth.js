4747afb347d6b4c5e862950f7d2df3cca87734d0
kazuto0291 <zutozuto03170291@gmail.com>
2020-10-14 03:30:56 +0900

    flattenDepth

---
+++ doril/doril2/flattenDepth.js
@@ -0,0 +1,25 @@
+function flattenDepth() {
+  
+}
+
+const array = [1, [2, [3, [4]], 5]];
+
+const result0 = flattenDepth(array);
+console.log('result0 : ', result0);
+// => [1, 2, [3, [4]], 5]
+
+const result1 = flattenDepth(array, 1);
+console.log('result1 : ', result1);
+// => [1, 2, [3, [4]], 5]
+
+const result2 = flattenDepth(array, 2);
+console.log('result2 : ', result2);
+// => [1, 2, 3, [4], 5]
+
+const result3 = flattenDepth(array, 3);
+console.log('result3 : ', result3);
+// => [1, 2, 3, 4, 5]
+
+const result4 = flattenDepth(array, 100);
+console.log('result4 : ', result4);
+// => [1, 2, 3, 4, 5]
