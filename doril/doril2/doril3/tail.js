610deb743352d5bbb4af03b2146c57ff1f230ab8
kazuto0291 <zutozuto03170291@gmail.com>
2020-10-19 03:16:06 +0900

    tail

---
+++ doril/doril2/doril3/tail.js
@@ -0,0 +1,10 @@
+function tail(array) {
+  const tailedArray = [];
+  for(let i = 1 ; i < array.length; i++) {
+    tailedArray.push(array[i]);
+  }
+  return tailedArray
+}
+
+console.log( tail([1, 2, 3]) );
+// => [2, 3]
