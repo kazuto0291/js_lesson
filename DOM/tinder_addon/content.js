80eb00a50368c279494555c770b4db691f413564
kazuto0291 <zutozuto03170291@gmail.com>
2020-10-19 03:15:08 +0900

    addon

--- DOM/tinder_addon/content.js
+++ DOM/tinder_addon/content.js
@@ -1,5 +1,13 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendRespose) {
   if (message.type === 'like') {
-    document.getElementsByClassName('button focus-button-style')[3].click()
+    let count = 0
+    const id = setInterval(function() {
+      document.getElementsByClassName('button focus-button-style')[3].click()
+      count++
+      if(count >= message.count) {
+        window.alert('終わったよ!')
+        clearInterval(id)
+      }
+    }, 3000)
   }
  })
