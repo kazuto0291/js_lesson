ca8115002c6477d62b404a53c2164a1dd84e32bc
kazuto0291 <zutozuto03170291@gmail.com>
2020-11-10 21:41:55 +0900

    copy Array

--- U-node/lesson1/play.js
+++ U-node/lesson1/play.js
@@ -5,10 +5,15 @@ const person = {
     console.log('Hi, I am ' +  this.name);
   }
 }
-console.log(person)
-person.greet();
+// console.log(person)
+// person.greet();
 
 const hobbies = ['Sports', 'Cooking', 'programing', 'english'];
-for (let hobby of hobbies) {
-  console.log(hobby);
-}
+// for (let hobby of hobbies) {
+//   console.log(hobby);
+// }
+const copiedArray = hobbies.slice();
+console.log(copiedArray)
+
+const copiedArray2 = [...hobbies];
+console.log(copiedArray2);
