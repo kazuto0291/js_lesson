574892d744212ec30a20e888021f5b0376b7ab3b
kazuto0291 <zutozuto03170291@gmail.com>
2020-11-10 21:54:45 +0900

    call-ob-propty

--- U-node/lesson1/play.js
+++ U-node/lesson1/play.js
@@ -5,6 +5,13 @@ const person = {
     console.log('Hi, I am ' +  this.name);
   }
 }
+
+const printName = ({ name }) => {
+  console.log(name);
+}
+
+printName(person);
+
 // console.log(person)
 // person.greet();
 
@@ -12,16 +19,16 @@ const hobbies = ['Sports', 'Cooking', 'programing', 'english'];
 // for (let hobby of hobbies) {
 //   console.log(hobby);
 // }
-const copiedArray = hobbies.slice();
-console.log(copiedArray)
+// const copiedArray = hobbies.slice();
+// console.log(copiedArray)
 
-const copiedArray2 = [...hobbies];
-console.log(copiedArray2);
+// const copiedArray2 = [...hobbies];
+// console.log(copiedArray2);
 
-const copiedOb = {...person};
-console.log(copiedOb);
+// const copiedOb = {...person};
+// console.log(copiedOb);
 
-const toArray = (...args) => {
-  return args
-}
-console.log(toArray(1,2,3,4,5,6,7,8,9))
+// const toArray = (...args) => {
+//   return args
+// }
+// console.log(toArray(1,2,3,4,5,6,7,8,9))
