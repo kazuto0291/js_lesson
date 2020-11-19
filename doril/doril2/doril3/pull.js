5b13827311d6e1233f5a3cdb83eb2d215d6ce2c1
kazuto0291 <zutozuto03170291@gmail.com>
2020-11-19 23:08:32 +0900

    Update pull.js

--- doril/doril2/doril3/pull.js
+++ doril/doril2/doril3/pull.js
@@ -1,8 +1,16 @@
-function pull() {
-  
+function pull(array, ...values) {
+  const pullArray = []
+  for(let i = 0; i < array.length; i++) {
+    const target = array[i];
+    if(!values.includes(target)) {
+      pullArray.push(target);
+    }
+  }
+  return pullArray;
 }
 
 const array = ['a', 'b', 'c', 'a', 'b', 'c'];
 pull(array, 'a', 'c');
 console.log(array);
 // => ['b', 'b']
+console.log(pull(array, 'a', 'c'))
