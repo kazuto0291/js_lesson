c374b0300ecd6e6bb59b1aaf6f7dd2689d2c7d50
kazuto0291 <zutozuto03170291@gmail.com>
2020-11-28 12:15:04 +0900

    eventemitter

--- U-node/prototype/indes.js
+++ U-node/prototype/indes.js
@@ -1,12 +1,15 @@
+const EventEmitter = require("events").EventEmitter;
 const util = require("util");
 
 const Animal = function () {
-
+  EventEmitter.call(this);
 };
+util.inherits(Animal, EventEmitter);
 
 Animal.prototype.message="qwert";
 Animal.prototype.say= function () {
   console.log(this.message);
+  this.emit("say", this.message);
 };
 
 const Dog = function () {
@@ -15,5 +18,9 @@ const Dog = function () {
 };
 
 util.inherits(Dog, Animal);
+
 const dog = new Dog();
+dog.on("say", function (message) {
+  console.log(`「${message}」と叫んだ`)
+});
 dog.say();
