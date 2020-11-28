f0622653858f78258146dfd8952f67e34df3999e
kazuto0291 <zutozuto03170291@gmail.com>
2020-11-28 12:08:00 +0900

    Create indes.js

---
+++ U-node/prototype/indes.js
@@ -0,0 +1,19 @@
+const util = require("util");
+
+const Animal = function () {
+
+};
+
+Animal.prototype.message="qwert";
+Animal.prototype.say= function () {
+  console.log(this.message);
+};
+
+const Dog = function () {
+  Animal.call(this);
+  this.message = "わんわん"
+};
+
+util.inherits(Dog, Animal);
+const dog = new Dog();
+dog.say();
