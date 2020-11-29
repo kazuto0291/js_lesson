998338286693e7c19b1a3f62f9c294b9468f13ba
kazuto0291 <zutozuto03170291@gmail.com>
2020-11-29 15:35:15 +0900

    collection表示

--- U-node/mongojs/index.js
+++ U-node/mongojs/index.js
@@ -4,7 +4,14 @@ const url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url,(error,client)=> {
   const db =client.db("sample");
-  db.createCollection("test4",(error,collection) => {
+  // db.createCollection("test4",(error,collection) => {
+  //   client.close();
+  // })
+
+  db.listCollections().toArray((error, items)=> {
+    for(let item of items) {
+      console.log(item.name);
+    }
     client.close();
   })
 });
