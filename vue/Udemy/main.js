c0c28a6caab5ad5c356ed709b5953c659d26cc6b
kazuto0291 <zutozuto03170291@gmail.com>
2020-12-16 23:34:40 +0900

    v-bind

--- vue/Udemy/main.js
+++ vue/Udemy/main.js
@@ -4,7 +4,14 @@ new Vue({
     message: "HElloWorld",
     number: 3,
     ok: false,
-    html: '<h1>h1です</h1>'
+    html: '<h1>h1です</h1>',
+    url: 'https://google.com',
+    urlTwitter: 'https://twitter.com',
+    attribute: 'href',
+    twitterObject: {
+      href: 'https://twitter.com',
+      id:31
+    }
   },
   methods: {
     reverseMessage: function() {
