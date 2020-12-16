9f58f0a916bacb90156e5cfdfbb04485ab2d6f9d
kazuto0291 <zutozuto03170291@gmail.com>
2020-12-16 23:13:47 +0900

    vue-lesson1

--- vue/Udemy/main.js
+++ vue/Udemy/main.js
@@ -1,11 +1,19 @@
 new Vue({
   el:'#app',
   data: {
-    message: "HElloWorld"
+    message: "HElloWorld",
+    number: 3,
+    ok: false,
+    html: '<h1>h1です</h1>'
   },
   methods: {
     reverseMessage: function() {
       this.message =this.message.split('').reverse().join('')
+    },
+    sayhi: function(){
+      this.message = " Hello Vuejs"
+      return this.message;
     }
   }
 })
+
