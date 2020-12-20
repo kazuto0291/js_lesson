60d10721909dd9b2627c696373aba93be2aa3ea3
kazuto0291 <zutozuto03170291@gmail.com>
2020-12-20 18:37:27 +0900

    computed
    データを動的にな値を扱うことはできない。あくまでも初期値を持つだけ。
    データを動的に扱うには、vueだとcomputedを使う

--- vue/Udemy/v-model/main.js
+++ vue/Udemy/v-model/main.js
@@ -1,6 +1,12 @@
 new Vue({
   el: '#app',
   data: {
-    message:'こんにちは'
+    message:'こんにちは',
+    counter: 0
+  },
+  computed: {
+    lessThanThree: function() {
+      return this.counter > 3 ? '3より上' : '3以下'
+    }
   }
 })
