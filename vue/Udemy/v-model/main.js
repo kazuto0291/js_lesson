5388de13011265fa2078f470785513b824ac0200
kazuto0291 <zutozuto03170291@gmail.com>
2020-12-20 18:50:32 +0900

    computed

--- vue/Udemy/v-model/main.js
+++ vue/Udemy/v-model/main.js
@@ -2,10 +2,18 @@ new Vue({
   el: '#app',
   data: {
     message:'こんにちは',
-    counter: 0
+    counter: 0,
+    otherCounter: 0
   },
   computed: {
-    lessThanThree: function() {
+    lessThanThreeComputed: function() {
+      console.log("Computed")
+      return this.counter > 3 ? '3より上' : '3以下'
+    }
+  },
+  methods: {
+    lessThanThreeMethod: function() {
+      console.log('Method')
       return this.counter > 3 ? '3より上' : '3以下'
     }
   }
