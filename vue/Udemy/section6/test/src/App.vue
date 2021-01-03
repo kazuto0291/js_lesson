44065cfd7771dbc036e4f4efb8e7a2771b8c9c2a
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-03 17:27:50 +0900

    vue-sass-install

--- vue/Udemy/section6/test/src/App.vue
+++ vue/Udemy/section6/test/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your"/>
+    <TestComponent />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import TestComponent from './components/modules/TestComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TestComponent
   }
 }
 </script>
