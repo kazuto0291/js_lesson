1e3c821e93896d541a7bc1b6bfc6a89d47ff6d30
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-21 03:10:49 +0900

    indexOf

---
+++ doril/doril2/doril3/doril4/indexOf.js
@@ -0,0 +1,21 @@
+function indexOf(array, value, fromIndex=0) {
+
+  for(let i = fromIndex; i < array.length; i++) {
+    const target = array[i];
+    if (target === value) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+
+
+console.log( indexOf([1, 2, 1, 2], 2) );
+// => 1
+
+// Search from the `fromIndex`.
+console.log( indexOf([1, 2, 1, 2], 2, 2) );
+// => 3
+console.log( indexOf([1, 2, 1, 2], 3, 2) );
+// => -1
